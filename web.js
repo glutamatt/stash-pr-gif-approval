0f44e1c711b2023fce4b7b4ebb41ed92e31b1528
Gluta <fatal974@hotmail.com>
2014-03-15 02:28:16 +0100

    Framwork tpl assets

--- web.js
+++ web.js
@@ -1,11 +1,23 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var fs = require('fs');
 var app = express();
+var Hogan = require('hogan.js');
 
 app.use(logfmt.requestLogger());
+app.use(express.static(__dirname + '/assets'));
+
+var renderTpl = function(tpl, data, cb) {
+	fs.readFile(__dirname + '/views/' + tpl, 'utf8', function (err,content) {
+		if (err) return console.log(err);
+		return cb(Hogan.compile(content).render(data));
+	});
+};
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+	renderTpl('index', {name:'matt'}, function(content){
+		res.send(content);		
+	});
 });
 
 var port = Number(process.env.PORT || 5000);
